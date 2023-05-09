affb7f2e0bf1a3192084b89623b5ea622b203a03
cmb2897 <cmb2897@gmail.com>
2023-05-08 19:05:47 -0600

    Adding Repository methods to BasketController. Adding Basket Repository and Interface. Fixing Redis Connection String. Getting Scoped Basket Repository Service.

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _repository;
+        public BasketController(IBasketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{userName}", Name = "GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> Get(string userName)
+        {
+            var basket = await _repository.GetBasket(userName);
+            return Ok(basket ?? new ShoppingCart(userName));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart shoppingCart) => Ok(await _repository.UpdateBasket(shoppingCart));
+
+        [HttpDelete("{userName}", Name = "DeleteBasket")]
+        public async Task<IActionResult> DeleteBasket(string userName) {
+            await _repository.DeleteBasket(userName);
+            return Ok();
+        }
+    }
+}
