95a96a7572737cd1249a8e58afd8edebab993382
cmb2897 <cmb2897@gmail.com>
2023-05-12 22:51:32 -0600

    Adding GRPC Project, a duplicate from Discount API mostly.

---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,29 @@
+﻿using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+    {
+        private readonly IDiscountRepository _repository;
+        private readonly ILogger<DiscountService> _logger;
+
+        public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repository.GetDiscount(request.ProductName);
+            if (coupon == null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with Product Name {request.ProductName} is not found."));
+            }
+
+            var couponModel = _mapper.Map<CouponModel>(coupon);
+            return couponModel;
+    }
+}
